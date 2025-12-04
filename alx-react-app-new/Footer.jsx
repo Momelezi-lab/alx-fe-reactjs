70d2227ec3733f488a461da72c7471c230d7de0c
Momelezi-lab <fuzilea@gmail.com>
2025-12-04 15:06:24 +0200

    added alx-react-app-new

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
